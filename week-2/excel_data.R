d09b37c06004fa944025246340e6bbcb8170b783
poojitha <poojithagatta9@gmail.com>
2024-02-25 10:43:25 +0530

    commit

---
+++ week-2/excel_data.R
@@ -0,0 +1,20 @@
+#IMPORTING EXCEL DATA INTO GIT
+#Working with excel sheets using R
+
+head(mental_health_data)
+YearStart YearEnd LocationAbbr LocationDesc DataSource         Topic
+1      2018    2018           NV       Nevada      BRFSS Mental Health
+2      2020    2020           NV       Nevada      BRFSS Mental Health
+3      2018    2018           NV       Nevada      BRFSS Mental Health
+4      2019    2019           NV       Nevada      BRFSS Mental Health
+5      2017    2017           NV       Nevada      BRFSS Mental Health
+6      2016    2016           NV       Nevada      BRFSS Mental Health
+
+tail(mental_health_data)
+YearStart YearEnd LocationAbbr LocationDesc DataSource         Topic
+13195      2020    2020           WY      Wyoming      BRFSS Mental Health
+13196      2020    2020           WY      Wyoming      BRFSS Mental Health
+13197      2020    2020           WY      Wyoming      BRFSS Mental Health
+13198      2020    2020           WY      Wyoming      BRFSS Mental Health
+13199      2020    2020           WY      Wyoming      BRFSS Mental Health
+13200      2020    2020           WY      Wyoming      BRFSS Mental Health
